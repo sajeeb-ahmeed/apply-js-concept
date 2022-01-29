5b81a2056002b9e785dd93f2c084c4b80b8de4b1
sajeeb-ahmeed <sajeeb.web@gmail.com>
2022-01-30 01:59:03 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,54 @@
+// hrs to min 
+
+
+// function hoursConvertToMin(hour) {
+
+//     let convertToMin = (hour * 60 + " Minutes");
+//     console.log(convertToMin);
+
+// }
+// // hoursConvertToMin(7);
+// let membersName = ["dadu", "nanu", "fufu", "baba", "ma"];
+
+// function namess() {
+//     for (var i = 0; i < membersName.length; i++) {
+
+//         let opinios = membersName[i];
+//         console.log(opinios);
+//     }
+// }
+// namess();
+// =====================
+// feet to inch 
+
+// function converInch(inche) {
+//     let feet = inche / 12;
+//     return feet;
+
+// }
+// var myInches = 144;
+// var feet = converInch(myInches);
+// console.log(feet);
+
+// var myInchess = 184;
+// var feet = converInch(myInchess);
+// console.log(feet)
+// ===========================
+
+// miles to kilo miters
+
+// function converMilesTokm(miles) {
+//     let km = miles * 1.60934
+//     return km;
+// }
+// var saju = 10;
+// var km = converMilesTokm(saju);
+// console.log(km);
+
+// var sakib = 26;
+// var kms = converMilesTokm(sakib);
+// console.log(kms);
+
+// var sakibs = 36;
+// var kms = converMilesTokm(sakibs);
+// console.log(kms);
