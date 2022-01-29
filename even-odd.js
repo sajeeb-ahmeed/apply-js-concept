9d88d19bb9bc6e271332527c153fc0c8471c0870
sajeeb-ahmeed <sajeeb.web@gmail.com>
2022-01-30 03:04:06 +0600

    odd function added

--- even-odd.js
+++ even-odd.js
@@ -1,14 +1,26 @@
-// even -odd check 
+// even - check 
 function isEven(number) {
     if (number % 2 == 0) {
         return true;
     }
     return false;
 }
+// odd -check 
+
+function isOdd(number) {
+    if (number % 2 == 1) {
+        return true;
+    }
+    return false;
+}
+
+const myNumber1 = 327823;
+const isEvanNumber1 = isOdd(myNumber1);
+console.log("This is Odd ", isEvanNumber1);
 
 const myNumber = 327823;
 const isEvanNumber = isEven(myNumber);
-console.log(isEvanNumber);
+console.log("This is Evan ", isEvanNumber);
 
 const herNumbers = 32782;
 const isEvanNumbers = isEven(herNumbers);
