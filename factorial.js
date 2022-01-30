dc4ccd45457fa5beeb7c1313b27d9421ffc45988
sajeeb-ahmeed <sajeeb.web@gmail.com>
2022-01-30 15:55:58 +0600

    multi time  factorial recalculation

--- factorial.js
+++ factorial.js
@@ -12,3 +12,25 @@ console.log(factorial)
 // 5! = 5 x 4 x 3 x 2 x 1 
 // 6! = 6 x  5 x 4 x 3 x 2 x 1 
 // 7! + 7 x 6 x  5 x 4 x 3 x 2 x 1 
+
+// recalculate factorial multiple time  function
+
+function getFatcorial(number) {
+    let factorial = 1;
+    for (i = 1; i <= number; i++) {
+        factorial = factorial * i
+    }
+    return factorial;
+}
+
+var myFact = 9;
+var sevenfact = getFatcorial(myFact);
+console.log(sevenfact);
+
+let yourFact = 5;
+let fiveFact = getFatcorial(yourFact);
+console.log(fiveFact);
+
+let theirFact = 13;
+let thirtyFactorial = getFatcorial(theirFact);
+console.log(thirtyFactorial);
