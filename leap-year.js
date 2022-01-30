06c2690a7e448d537839f963ea879317a216b197
sajeeb-ahmeed <sajeeb.web@gmail.com>
2022-01-30 15:10:30 +0600

    leapYear

--- leap-year.js
+++ leap-year.js
@@ -1,14 +1,15 @@
 function leap(year) {
-    if (year % 4 == 0) {
+
+    if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)) {
         return true;
     } else {
         return false;
     }
 }
-const myYear = 2024;
+const myYear = 2100;
 const checkLeap = leap(myYear);
 console.log(checkLeap);
 
-const yourYear = 2033;
+const yourYear = 2032;
 const result = leap(yourYear);
 console.log(result)
