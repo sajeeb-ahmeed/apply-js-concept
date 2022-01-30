4502cac69ee345abf38f098274704592d8a1f5d3
sajeeb-ahmeed <sajeeb.web@gmail.com>
2022-01-30 15:30:15 +0600

    factorial.js

--- factorial.js
+++ factorial.js
@@ -5,3 +5,10 @@ for (let i = 1; i <= 7; i++) {
 }
 console.log(factorial)
 // factorial value assign 7! 
+// 1! = 1 x 1
+// 2! = 2 x 1 
+// 3! = 3 x 2 x 1 
+// 4! = 4 x 3 x 2 x 1 
+// 5! = 5 x 4 x 3 x 2 x 1 
+// 6! = 6 x  5 x 4 x 3 x 2 x 1 
+// 7! + 7 x 6 x  5 x 4 x 3 x 2 x 1 
